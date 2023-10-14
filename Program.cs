eb0f2ed00e87d0d44d8ea773afaff2121f376d77
kindyy77 <63909055+kindyy77@users.noreply.github.com>
2023-10-14 16:26:26 +0300

    практос4 ежедневник

--- Program.cs
+++ Program.cs
@@ -1,114 +1,128 @@
-﻿
-int choice;
-do
- {
-    Console.WriteLine("Выберите действие :");
-    Console.WriteLine("1. Сложить 2 числа");
-    Console.WriteLine("2. Вычесть первое из второго");
-    Console.WriteLine("3. Перемножить два числа");
-    Console.WriteLine("4. Разделить первое на второе");
-    Console.WriteLine("5. Возвести в степень N первое число");
-    Console.WriteLine("6. Найти квадратный корень из числа");
-    Console.WriteLine("7. Найти 1 процент от числа");
-    Console.WriteLine("8. Найти факториал из числа");
-    Console.WriteLine("9. Выйти из программы");
-    choice = Convert.ToInt32(Console.ReadLine());
-        switch (choice)
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Note> notes = new List<Note>()
         {
-            case 1:
-                Console.WriteLine("Введите первое число :");
-                string x = Console.ReadLine();
-                int a = Convert.ToInt32(x);
 
-                Console.WriteLine("Введите второе число :");
-                string r = Console.ReadLine();
-                int b = Convert.ToInt32(r);
+        };
 
-                int sum = a + b;
-            Console.WriteLine($"Сумма чисел {a} и {b} равна {sum}");
-                break;
-            case 2:
-                Console.WriteLine("Введите первое число :");
-            string h = Console.ReadLine();
-            int g = Convert.ToInt32(h);
-                Console.WriteLine("Введите второе число :");
-            string l = Console.ReadLine();
-            int o = Convert.ToInt32(l);
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Ежедневник");
+            Console.WriteLine("1. Просмотреть заметки");
+            Console.WriteLine("2. Добавить новую заметку");
+            Console.WriteLine("3. Выйти");
 
-            int c = g - o;
-            Console.WriteLine($"Разность чисел {g} и {o} равна {c}");
-                break;
-            case 3:
-                Console.WriteLine("Введите первое число :");
-            string ab = Console.ReadLine();
-            int oa = Convert.ToInt32(ab);
-                Console.WriteLine("Введите второе число :");
-            string hg = Console.ReadLine();
-            int yu = Convert.ToInt32(hg);
+            ConsoleKey key = Console.ReadKey(true).Key;
 
-            int yt = oa * yu;
-            Console.WriteLine($"Произведение чисел {oa} и {yu} равно {yt}");
-            break;
-            case 4:
-                Console.WriteLine("Введите первое число :");
-            string kl = Console.ReadLine();
-            int it = Convert.ToInt32(kl);
-                Console.WriteLine("Введите второе число :");
-            string gk = Console.ReadLine();
-            int pl = Convert.ToInt32(gk);
-
-            int nm = it / pl;
-            Console.WriteLine(nm);
-            Console.WriteLine($"Частное будет равно {nm}");
-            break;
-            case 5:
-                Console.WriteLine("Введите первое число:");
-            string input1 = Console.ReadLine();
-            double number1 = Convert.ToDouble(input1);
-
-                    Console.WriteLine("Введите степень:");
-            string input2 = Console.ReadLine();
-            int exponent = Convert.ToInt32(input2);
-
-            double result = Math.Pow(number1, exponent);
-            Console.WriteLine($"Степень от числа {number1} равна {result}");
-            break;
-            case 6:
-                Console.WriteLine("Введите число:");
-            string input = Console.ReadLine();
-            double hfds = double.Parse(input);
-            double squareRoot = Math.Sqrt(hfds);
-            Console.WriteLine(squareRoot);
-            Console.WriteLine($"Квадратный корень {hfds} равен {squareRoot}");
-            break;
-            case 7:
-            Console.WriteLine("Введите число");
-            string input3 = Console.ReadLine();
-            double kjg = double.Parse(input3);
-            double onePercent = kjg / 100;
-            Console.WriteLine($"1% от числа {kjg} равно {onePercent}");
-            break;
-            case 8:
-            Console.WriteLine("Введите число:");
-            string input6 = Console.ReadLine();
-            int number5 = int.Parse(input6);
-            static int Factorial(int ajfjn)
+            switch (key)
             {
-                int factorial = 1;
-                for (int fdakfl = 2; fdakfl <= ajfjn; fdakfl++)
-                {
-                    factorial *= fdakfl;
-                }
-                return factorial;
+                case ConsoleKey.D1:
+                    ViewNotes(notes);
+                    break;
+                case ConsoleKey.D2:
+                    AddNewNote(notes);
+                    break;
+                case ConsoleKey.D3:
+                    return;
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.DownArrow:
+                    break;
+                default:
+                    Console.WriteLine("Неверный ввод. Пожалуйста, выберите пункт меню, нажав соответствующую цифру или используя стрелки.");
+                    break;
             }
-            Console.WriteLine(Factorial(number5));
-            break;
-            case 9:
-            break;
-        default:
-                Console.WriteLine("Некорректный выбор.");
-                break;
+        }
+    }
+
+    static void ViewNotes(List<Note> notes)
+    {
+        int selectedNoteIndex = 0;
+        bool noteSelected = false; 
+
+        while (!noteSelected)
+        {
+            Console.Clear();
+            Console.WriteLine("Список заметок:");
+
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (i == selectedNoteIndex)
+                    Console.WriteLine($"> {i + 1}. {notes[i].Title}");
+                else
+                    Console.WriteLine($"  {i + 1}. {notes[i].Title}");
+            }
+
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    selectedNoteIndex = Math.Max(selectedNoteIndex - 1, 0);
+                    break;
+                case ConsoleKey.DownArrow:
+                    selectedNoteIndex = Math.Min(selectedNoteIndex + 1, notes.Count - 1);
+                    break;
+                case ConsoleKey.Enter:
+                    if (selectedNoteIndex >= 0 && selectedNoteIndex < notes.Count)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Подробная информация о заметке");
+                        Console.WriteLine("Название: " + notes[selectedNoteIndex].Title);
+                        Console.WriteLine("Описание: " + notes[selectedNoteIndex].Description);
+                        Console.WriteLine("Дата: " + notes[selectedNoteIndex].Date.ToShortDateString());
+                        Console.WriteLine("Нажмите любую клавишу, чтобы вернуться к списку заметок...");
+                        Console.ReadKey();
+                    }
+                    noteSelected = true;
+                    break;
+                default:
+                    Console.WriteLine("Неверный ввод. Пожалуйста, выберите номер заметки стрелками и нажмите Enter для просмотра подробной информации.");
+                    break;
+            }
+        }
+    }
+
+    static void AddNewNote(List<Note> notes)
+    {
+        Console.Clear();
+        Console.WriteLine("Добавление новой заметки");
+        Console.WriteLine("Введите название заметки:");
+        string title = Console.ReadLine();
+
+        Console.WriteLine("Введите описание заметки:");
+        string description = Console.ReadLine();
+
+        Console.WriteLine("Введите дату заметки (гггг-мм-дд):");
+        DateTime date;
+        while (!DateTime.TryParse(Console.ReadLine(), out date))
+        {
+            Console.WriteLine("Неверный формат даты. Пожалуйста, введите дату в формате гггг-мм-дд:");
         }
 
-        Console.WriteLine();
-    } while (choice != 9);
+        Note newNote = new Note(title, description, date);
+        notes.Add(newNote);
+        Console.WriteLine("Заметка успешно добавлена!");
+        Console.WriteLine("Нажмите любую клавишу, чтобы вернуться к меню...");
+        Console.ReadKey();
+    }
+}
+
+class Note
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+
+    public Note(string title, string description, DateTime date)
+    {
+        Title = title;
+        Description = description;
+        Date = date;
+    }
+}
